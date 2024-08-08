ffbe542417b516c3872d3d0c6ac9651d589d88b0
webber0928 <bird23074035@gmail.com>
2024-08-08 11:46:24 +0800

    Feature: 建立一個demo code

--- README.md
+++ README.md
@@ -11,10 +11,19 @@ python 3.9.x
 python3 -m venv env
 source env/bin/activate  # On Windows use `env\Scripts\activate`
 
+# 離開 env
+deactivate
+
 # 使用單一應用程式設定一個新項目
 django-admin startproject tutorial .
 cd tutorial
 django-admin startapp quickstart
+
+# 同步資料庫
+python manage.py migrate
+
+# 建立初始 admin
+python manage.py createsuperuser --username admin --email bird23074035@gmail.com # 0000
 
 
 ```
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
